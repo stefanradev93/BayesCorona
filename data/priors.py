de3595babefc65d9fb3f4be35ae7fdbe99a4bc07
Christoph Straehle <cstraehle@gmail.com>
2020-03-31 11:38:12 +0000

    change priors a little

--- data/priors.py
+++ data/priors.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-rh_names = [r'atanh-rho1',r'atanh-rho2',r'atanh-rho3',r'log-t1',r'log-t2',r'log-t3',r'log-t4']
+rh_names = [r'rho1',r'rho2',r'rho3',r't1',r't2',r't3',r't4']
 th_names = [r'E0', r'p', r'$\beta$', r'$\alpha$', r'$\gamma$', r'$d$']
 
-th_low = [1,0.01, 0.8, 0.05, 0.05, 0.001]
-th_high=[100, 1.0, 3.25, 0.3, 0.3, 0.1]
+th_low = [1,0.01, 0.8, 0.05, 0.05, 0.0001]
+th_high=[100, 1.0, 3.25, 0.7, 0.3, 0.05]
 rh_low = [0,0,0,0,0,0,0]
 rh_high = [1,1,1,1,1,1,1]
 

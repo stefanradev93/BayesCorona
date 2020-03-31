991db964d6c1bd44ab24d2cdf095098bba506c62
Christoph Straehle <cstraehle@gmail.com>
2020-03-31 11:37:57 +0000

    refactor + predict curves of estimated parameters

--- data/data.py
+++ data/data.py
@@ -8,7 +8,8 @@ Created on Mon Mar 30 14:32:30 2020
 
 import numpy as np
 import pandas as pd
-
+import data.priors as priors
+from deep_bayes import theta as thetaed
 
 def load_country(S0,country="Germany"):
     Idf = pd.read_csv("data/time_series_covid19_confirmed_global_narrow.csv",skiprows=[1],parse_dates=["Date"],index_col="Date")
@@ -46,7 +47,7 @@ def enhance_contrast(x):
     return np.concatenate([a,b,t],axis=2)
 
 def add_counting_noise(x,N,p):
-    # have binomial counting noise on infected rate
+    # have binomial counting noise on delta infected rate
     delta_I = x[:,1:,1]-x[:,:-1,1]
     dIcounts = (delta_I*N).astype(np.int64)
     new_deltas = np.random.binomial(np.abs(dIcounts),p)
@@ -59,6 +60,75 @@ def add_counting_noise(x,N,p):
     x[:,1:,-1] *= Rfactors
     
     return x
+
+
+#@jit(parallel=True,nopython=False)
+def forward_model(aparams, t,psteps=0,init_params=None):
+    """Forward model of the SIRD."""
+    result = np.zeros((aparams.shape[0],t.shape[0]+psteps,4))
+    offset = 0
+    if init_params is None:
+        offset = 5
+        
+    arh_params = thetaed.decode(aparams[:,offset+0:offset+7],priors.rh_low, priors.rh_high)
+    ath_params = thetaed.decode(aparams[:,offset+7:],priors.th_low, priors.th_high)
+    at = np.repeat(t[None,:],aparams.shape[0],axis=0)
+    
+    for pn in range(aparams.shape[0]):
+        #print(pn)
+        if init_params is None:
+            N, S_0, I_0, D_0, R_0 = aparams[pn,:5]
+        else:
+            N, S_0, I_0, D_0, R_0 = init_params
+            
+        S, I, D, R = [S_0], [I_0], [D_0], [R_0]
+
+        #extract time dependent rhos from params
+        rh_params = arh_params[pn]
+        t = at[pn]
+        
+        rhos = rh_params[0:3]
+        rhos_t = rh_params[3:7]
+        rhos_t = rhos_t / np.sum(rhos_t)
+        rhos_t = np.cumsum(rhos_t)[0:3]*t.shape[0]
+        rhos_t = np.clip(rhos_t.astype(np.int64),0,t.shape[0])
+
+        #construct dense rhos array
+        rho = np.ones((t.shape[0]))
+        for i in range(rhos_t.shape[0]):
+            rho[rhos_t[i]:] = rhos[i]   
+        th_params = ath_params[pn]
+        E_0, p, beta, alpha, gamma, d = th_params
+        E = [E_0/N]
+
+        dt = t[1] - t[0]
+        #print(rho, th_params)
+        
+        if psteps != 0:
+            pt = np.linspace(t[-1], t[-1]+dt*psteps, psteps)
+            t = np.concatenate([t,pt])
+            rhop = np.ones(pt.shape)*rho[-1]
+            rho = np.concatenate([rho,rhop])
+
+        for i,_ in enumerate(t[1:]):
+            next_S = S[-1] + (-rho[i]*beta*S[-1]*I[-1])*dt
+            next_E = E[-1] + (rho[i]*beta*S[-1]*I[-1] - alpha*E[-1])*dt
+            next_I = I[-1] + (alpha*E[-1] - gamma*I[-1] - d*I[-1])*dt
+            next_R = R[-1] + (gamma*I[-1])*dt
+            next_D = D[-1] + (d*I[-1])*dt
+            
+            #print(I[-1],next_R, next_D, next_D - D[-1],d)
+
+            S.append(next_S)
+            E.append(next_E)
+            I.append(next_I)
+            D.append(next_D)
+            R.append(next_R)
+
+        result[pn,...] = np.stack([S, I, D, R]).T
+        #print("timeseries", dt, th_params, result[pn])
+    return result
+
 
 if __name__ == "__main__":    
     S,I,R,D = load_country(S0=82000000, country = "Germany")
